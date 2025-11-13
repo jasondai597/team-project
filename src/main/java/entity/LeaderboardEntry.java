2086e9779daa51ef753d6e36f510683d01593bf0
pandaHasan <hasanjarrashofficial@gmail.com>
2025-11-13 18:03:25 -0500

    Finished initial DAO

---
+++ src/main/java/entity/LeaderboardEntry.java
@@ -0,0 +1,18 @@
+package entity;
+
+public class LeaderboardEntry {
+    private final String userId;
+    private final String username;
+    private final String difficulty;
+    private final long timeMs;
+
+    public LeaderboardEntry(String userId, String username,
+                            String difficulty, long timeMs) {
+        this.userId = userId;
+        this.username = username;
+        this.difficulty = difficulty;
+        this.timeMs = timeMs;
+    }
+
+    // getters...
+}
