b78f25d546c0c0d9f4064e5c3f00b37a2f09a2ea
Jason Dai <jasondai@Jasons-MacBook-Air-9.local>
2025-11-21 17:37:10 -0500

    Implemented StartGameInteractor, which uses GameDataAccess

--- src/main/java/data_access/FirebaseInitialize.java
+++ src/main/java/data_access/FirebaseInitialize.java
@@ -1,36 +1,40 @@
 package data_access;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.auth.oauth2.GoogleCredentials;
+
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class FirebaseInitialize {
+
     private static boolean initialized = false;
 
-    private FirebaseInitialize(){}
-
     public static void initFirebase() {
-        if (initialized) return; // avoid double initialization
+        if (initialized) return;
+
         try {
             FileInputStream serviceAccount =
                     new FileInputStream("src/main/java/data_access/firebaseService.json");
 
-            FirebaseOptions options = new FirebaseOptions.Builder()
+            FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://sudokuthing.firebaseio.com/")
                     .build();
 
             FirebaseApp.initializeApp(options);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            initialized = true;
+
+            System.out.println("Firebase initialized.");
+        } catch (IOException e) {
+            throw new RuntimeException("Firebase init error: " + e.getMessage());
         }
-        initialized = true;
     }
 
     public static FirebaseDatabase getDatabase() {
+        initFirebase();
         return FirebaseDatabase.getInstance();
     }
-
 }
