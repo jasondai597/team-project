1ff321fe0e9cf5cbf9b9c2c9e7649360e4587253
Jason Dai <jasondai@Jasons-MacBook-Air-9.local>
2025-11-24 13:52:21 -0500

    finished Save on quit feature

--- src/main/java/interface_adapter/SudokuController.java
+++ src/main/java/interface_adapter/SudokuController.java
@@ -3,15 +3,19 @@ package interface_adapter;
 import use_case.LoadingSudoku.LoadSudokuInputBoundary;
 import use_case.LoadingSudoku.LoadSudokuInteractor;
 import use_case.LoadingSudoku.LoadSudokuInputData;
+
 public class SudokuController {
     private final LoadSudokuInputBoundary interactor;
+
     public SudokuController(LoadSudokuInteractor interactor) {
         this.interactor = interactor;
     }
 
     public void loadPuzzle(String difficulty) {
-
         interactor.execute(new LoadSudokuInputData(difficulty));
     }
 
+    public void saveGame(int[][] currentBoard) {
+        interactor.saveCurrentGameState(currentBoard);
+    }
 }
