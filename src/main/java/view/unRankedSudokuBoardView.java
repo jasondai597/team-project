8d5dae33571aeca2e65bc24aa21b980ea7207b84
mayozesoup <dackhang.nguyen@gmail.com>
2025-11-27 01:24:35 -0500

    tweaked the logic for the forfeit button, goes directly to main after forfeiting

--- src/main/java/view/unRankedSudokuBoardView.java
+++ src/main/java/view/unRankedSudokuBoardView.java
@@ -17,8 +17,8 @@ public class unRankedSudokuBoardView extends JPanel implements ActionListener, P
     private final SudokuBoardViewModel viewModel;
     private final SudokuController controller;
     private final hintController hint;
-    private final ForfeitController forfeitController;
-    private final processController process;
+//    private final ForfeitController forfeitController;
+//    private final processController process;
     private final CheckController check;
     private final ViewManagerModel viewManagerModel;
     private final String viewName = "unranked";
@@ -164,6 +164,8 @@ public class unRankedSudokuBoardView extends JPanel implements ActionListener, P
                     System.out.println("Saved game on quit!");
                     JOptionPane.showMessageDialog(this,
                             "You forfeited the game! Progress saved.");
+                    viewManagerModel.setState("main");
+                    viewManagerModel.firePropertyChange();
                 }
                 break;
         }
