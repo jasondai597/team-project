40e53c2f881d379fc104e2f0be2f404d4f4d7054
pandaHasan <hasanjarrashofficial@gmail.com>
2025-11-30 14:46:22 -0500

    Fixed Checkstyle issues in loading sudoku use case

--- src/main/java/use_case/LoadingSudoku/LoadSudokuInputBoundary.java
+++ src/main/java/use_case/LoadingSudoku/LoadSudokuInputBoundary.java
@@ -1,5 +1,14 @@
 package use_case.LoadingSudoku;
 
+/**
+ * The Input Boundary for loading a sudoku.
+ *
+ */
 public interface LoadSudokuInputBoundary {
+    /**
+     * The method to execute the loading use case.
+     * @param loadSudokuInputData
+     *      defines the input that the interactor takes.
+     */
     void execute(LoadSudokuInputData loadSudokuInputData);
 }
