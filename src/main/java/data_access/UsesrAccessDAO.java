9adce2716e2d8c4be451238c89c3a83e60839235
mayozesoup <dackhang.nguyen@gmail.com>
2025-11-29 23:24:36 -0500

    cleaned up todos in UserAccessDAO, added fetch classes and score handling classes

--- src/main/java/data_access/UsesrAccessDAO.java
+++ src/main/java/data_access/UsesrAccessDAO.java
@@ -7,6 +7,11 @@ import com.google.firebase.cloud.FirestoreClient;
 
 import java.util.concurrent.ExecutionException;
 import java.util.Map;
+
+//import classes from entity
+import entity.User;
+import entity.UserFactory;
+
 public class UsesrAccessDAO {
 
     private final CollectionReference userRef;
@@ -46,7 +51,40 @@ public class UsesrAccessDAO {
         return exist;
     }
 
-    // TODO create a fetch user data method that fetches the user data returns it as a user entity
-    // TODO create a method that updates the user data
-    // TODO optional, maybe overload the adduser function to be able to add more than just username and password
+    // add a score given the type (unranked or ranked + difficulty) ex: unranked hard, ranked easy
+    // Time will be an object for now. Can be changed later.
+    public void add_score(String username, Object time, String type) {
+        Map <String, Object> data = new java.util.HashMap<>();
+        data.put(type, time);
+        ApiFuture<WriteResult> future = userRef.document(username).update(data);
+        try {
+            future.get();
+            System.out.println(type + ": " + time + "time added for " + username);
+        }
+        catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            throw new RuntimeException("User doesn't exist, or failed to communicate with firebase");
+        }
+    }
+    public Object get_score(String username, String type) {
+        ApiFuture<DocumentSnapshot> future = userRef.document(username).get();
+        try {
+            DocumentSnapshot snapshot = future.get();
+            return snapshot.get(type); //Returns null if the field doesn't exist'
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            throw new RuntimeException("User doesn't exist, or failed to communicate with firebase");
+        }
+    }
+    // Get user class that returns a User object based on the user
+    public User getUser(String username, UserFactory userFactory) {
+        ApiFuture<DocumentSnapshot> future = userRef.document(username).get();
+        try {
+            DocumentSnapshot snapshot = future.get();
+            return userFactory.createUser(username, (String) snapshot.get("password"));
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            throw new RuntimeException("User doesn't exist, or failed to communicate with firebase");
+        }
+    }
 }
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,18 +1,19 @@
 package entity;
 
 public class User {
-    private final String userId;
     private final String username;
     private final String password;
 
-    public User(String username, String password, String userId) {
+    public User(String username, String password) {
+        if ("".equals(username)) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if ("".equals(password)) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
         this.username = username;
         this.password = password;
-        this.userId = userId;
-    }
 
-    public String getUserId() {
-        return userId;
     }
     public String getUsername() {
         return username;
